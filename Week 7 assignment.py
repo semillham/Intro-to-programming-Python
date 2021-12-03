2a75bf8ce1eac31f5828c469fd2e83e273d7d231
semillham <87036426+semillham@users.noreply.github.com>
2021-12-03 14:53:01 -0500

    Add files via upload
    Week 7 assignment for converting to Celsius.

---
+++ Week 7 assignment.py
@@ -0,0 +1,66 @@
+#assignment 6 for week 7
+#program has to take a series of 10 numbers in celsius to 10 values in Fahrenheit
+#then program has to decide whether the temperature is hot, cold, or nice.
+# set variables
+Feh = []
+x = len(Feh)
+Cel = eval(input('Please submit the values for celsius: '))
+# while statement to form loop
+while (len(Feh) != 10):
+    Feh.append(Cel)
+    Cel = eval(input('Please submit the values for celsius: '))
+#calculate result
+print((Feh[0] * 1.8) + 32)
+if Feh[0] < 11:
+    print('It is cold')
+elif Feh[0] > 25:
+    print('It is hot')
+print((Feh[1] * 1.8) + 32)
+if Feh[1] < 11:
+    print('It is cold')
+elif Feh[1] > 25:
+    print('It is hot')
+print((Feh[2] * 1.8) + 32)
+if Feh[2] < 11:
+    print('It is cold')
+elif Feh[2] > 25:
+    print('It is hot')
+print((Feh[3] * 1.8) + 32)
+if Feh[3] < 11:
+    print('It is cold')
+elif Feh[3] > 25:
+    print('It is hot')
+print((Feh[4] * 1.8) + 32)
+if Feh[4] < 11:
+    print('It is cold')
+elif Feh[4] > 25:
+    print('It is hot')
+print((Feh[5] * 1.8) + 32)
+if Feh[5] < 11:
+    print('It is cold')
+elif Feh[5] > 25:
+    print('It is hot')
+print((Feh[6] * 1.8) + 32)
+if Feh[6] < 11:
+    print('It is cold')
+elif Feh[6] > 25:
+    print('It is hot')
+print((Feh[7] * 1.8) + 32)
+if Feh[7] < 11:
+    print('It is cold')
+elif Feh[7] > 25:
+    print('It is hot')
+print((Feh[8] * 1.8) + 32)
+if Feh[8] < 11:
+    print('It is cold')
+elif Feh[8] > 25:
+    print('It is hot')
+print((Feh[9] * 1.8) + 32)
+if Feh[9] < 11:
+    print('It is cold')
+elif Feh[9] > 25:
+    print('It is hot')
+
+    
+    
+    
